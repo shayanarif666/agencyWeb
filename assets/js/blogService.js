6ef08615c77612ced86bd13cf3753a96bc0f859a
Muhammad Shayan <shayanarif666@gmail.com>
2024-11-11 19:09:29 +0500

    Login Functionality

---
+++ assets/js/blogService.js
@@ -0,0 +1,26 @@
+export class BlogService {
+    // Constructor Function
+    constructor() {}
+
+    // Get Blogs List
+    async getBlogs() {}
+
+    // Get Single Blog
+    async getBlog(id) {}
+
+    // Get Blogs By Pagination
+    async getBlogsByPagination(page = 1) {}
+
+    // Get Categories
+    async getCategories() {}
+
+    // Add Blog
+    async addNewBlog(data) {}
+
+    // Update Blog 
+    async updateBlog(data, id) {}
+
+    // Delete Blog 
+    async deleteBlog(id) {}
+
+}
